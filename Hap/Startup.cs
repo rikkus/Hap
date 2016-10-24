e670c89b7ac9a6fe97dd3b54cb6db029f4d95baa
rik <rik@DEV>
2016-10-24 00:03:22 +0000

    Add project files.

---
+++ Hap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hap.Startup))]
+namespace Hap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
